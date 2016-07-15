1b57bc9f981324de88b133dbec10d809ed3fac2e
Jaroslaw.Ogrodnik <jaroslaw.ogrodnik@wp.pl>
2016-07-15 14:19:01 +0200

    Creation of second CA1

--- run1.sh
+++ run1.sh
@@ -8,9 +8,9 @@ docker run -h ca1 -it  \
         --env CA_DEFAULT_countryName="PL" \
         --env CA_DEFAULT_stateOrProvinceName="Opolskie" \
         --env CA_DEFAULT_localityName="Opole" \
-        --env CA_DEFAULT_organizationName="Asseco Poland S.A." \
+        --env CA_DEFAULT_organizationName="Fishsoft Poland S.A." \
         --env CA_DEFAULT_organizationalUnitName="PRK" \
-        --env CA_DEFAULT_commonName="Asseco Poland S.A PRK (%s) CA" \
+        --env CA_DEFAULT_commonName="Fishsoft Poland S.A PRK (%s) CA" \
         --env CA_DEFAULT_emailAddress="jaroslaw.ogrodnik@asseco.pl" \
         --volume $(pwd)/ca1:/home/ca \
         --volume $(pwd)/assets/conf:/etc/docker-ca \
